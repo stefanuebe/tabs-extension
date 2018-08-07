c7880858360c5e51bb691f3c4ad0ed6fb35a8dd6
Stefan Uebe <stefan@vaadin.com>
2018-08-07 15:31:20 +0200

    Demos

--- demo/src/main/java/org/vaadin/stefan/tabs/demo/DemoView.java
+++ demo/src/main/java/org/vaadin/stefan/tabs/demo/DemoView.java
@@ -1,8 +1,16 @@
 package org.vaadin.stefan.tabs.demo;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.Registration;
+import org.vaadin.stefan.tabs.CloseableTab;
+import org.vaadin.stefan.tabs.TabsExtension;
+
+import java.util.stream.IntStream;
 
 /**
  * The main view contains a button and a template element.
@@ -12,6 +20,17 @@ import com.vaadin.flow.router.Route;
 public class DemoView extends VerticalLayout {
 
 	public DemoView() {
+		Tabs tabs = new Tabs();
+		add(tabs);
+		IntStream.range(0, 10)
+				.mapToObj(value -> new CloseableTab("Tab " + value))
+				.forEach(tabs::add);
+
+		tabs.addSelectedChangeListener(event -> Notification.show("Selected " + tabs.getSelectedIndex()));
+
+		Registration filterForTabs = TabsExtension.createFilterForTabs(tabs, CloseableTab.TABS_FILTER);
+
+//		filterForTabs.remove();
 	}
 
 }
