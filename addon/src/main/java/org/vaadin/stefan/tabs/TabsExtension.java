d92a1c73248c737013accd59f1db12baeb05bae6
Stefan Uebe <stefan@vaadin.com>
2018-08-07 15:32:11 +0200

    Method to disable key selection of tabs. Auto conversion of given filter strings.

--- addon/src/main/java/org/vaadin/stefan/tabs/TabsExtension.java
+++ addon/src/main/java/org/vaadin/stefan/tabs/TabsExtension.java
@@ -3,12 +3,18 @@ package org.vaadin.stefan.tabs;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.shared.Registration;
 
+import java.util.Arrays;
+
 public class TabsExtension {
-	public static Registration createFilterForTabs(Tabs tabs, String[] elements) {
+	public static Registration createFilterForTabs(Tabs tabs, String... tagsToFilter) {
 		tabs.getElement().getNode().runWhenAttached(ui -> {
+			String[] convertedTagsToFilter = Arrays.stream(tagsToFilter).map(String::toUpperCase).toArray(String[]::new);
+			String joinedFilter = String.join("','", convertedTagsToFilter);
+
+
 			ui.getPage().executeJavaScript("$0._superOnClick = $0._onClick;", tabs);
 			ui.getPage().executeJavaScript("$0._onClick = function(event) {" +
-					"   if(!Array.of('" + String.join("','",elements) + "').includes(event.target.tagName)) {" +
+					"   if(!Array.of('" + joinedFilter + "').includes(event.target.tagName)) {" +
 					"       $0._superOnClick(event);" +
 					"   }" +
 					"};", tabs);
@@ -18,7 +24,27 @@ public class TabsExtension {
 		return () -> {
 			tabs.getElement().getNode().runWhenAttached(ui -> {
 				ui.getPage().executeJavaScript("$0._onClick = $0._superOnClick;", tabs);
-				ui.getPage().executeJavaScript("$0._superOnClick = undefined; ");
+				ui.getPage().executeJavaScript("$0._superOnClick = undefined; ", tabs);
+			});
+		};
+	}
+
+	public static Registration disableKeySelectionOfTabs(Tabs tabs) {
+		tabs.getElement().getNode().runWhenAttached(ui -> {
+
+			ui.getPage().executeJavaScript("$0._superOnKeydown = $0._onKeydown; ", tabs);
+			ui.getPage().executeJavaScript("$0._onKeydown = function(event) {" +
+					"   const key = event.key.replace(/^Arrow/, '');" +
+					"   if(key.length != 1) {" +
+					"       $0._superOnKeydown(event);" +
+					"   }" +
+					"}; ", tabs);
+		});
+
+		return () -> {
+			tabs.getElement().getNode().runWhenAttached(ui -> {
+				ui.getPage().executeJavaScript("$0._onKeydown = $0._superOnKeydown;", tabs);
+				ui.getPage().executeJavaScript("$0._superOnKeydown = undefined; ", tabs);
 			});
 		};
 	}
